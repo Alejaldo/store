13a562ecac01d4b624c1f36550e7381007a50278
Alejaldo <statfluct@yahoo.com>
2021-03-12 12:12:04 +0500

    Update cheque ouptut

--- lib/cart.rb
+++ lib/cart.rb
@@ -10,4 +10,22 @@ class Cart
     @content << product
     @cost += product.price
   end
+
+  def cheque
+    quantity = @content.map do |product|
+      product.show_without_stock
+    end.tally
+
+    cheque_hash = Hash.new(0)
+
+    @content.each do |product|
+      cheque_hash[product.show_without_stock] = [product.price, quantity[product.show_without_stock]]
+    end
+
+    cheque_output = cheque_hash.map.with_index(1) do |(product, params), index|
+      "#{index}) -- #{product} | #{params[0]} * #{params[1]}    = #{params[0] * params[1]} руб. в т.ч. НДС"
+    end
+
+    return cheque_output
+  end
 end
--- main.rb
+++ main.rb
@@ -21,26 +21,23 @@ until user_input == 0
   while !(0..collection.size).include?(user_input)
     puts "Введенное значение некорректно! Введите цирфы от 0 до #{collection.size}"
     user_input = STDIN.gets.to_i
-  end  
+  end
 
   selected_product = collection[user_input - 1]
 
   if selected_product.stock == 0
     puts "Купите что-нибудь другое, этого уже у нас нет!"
-  else 
+  else
     selected_product.stock -= 1
     cart.add(selected_product)
   end
 
   puts "\nВы выбрали: #{selected_product.show_without_stock}\n\n"
-  puts "Всего товаров на сумму: #{cart.cost}\n\n"
+  puts "Всего товаров на сумму: #{cart.cost}руб. в т.ч. НДС\n\n"
 end
 
-cheque_list = cart.content.map { |product| [product.show_without_stock, product.price.to_i] }
-
-cheque_hash = Hash.new(0)
-cheque_list.each { |product_params| cheque_hash[product_params[0]] += product_params[1] }
-
 puts "Вы купили:\n\n"
-cheque_hash.each { |key, value| puts "Набор #{key} -- за #{value} руб. в т.ч. НДС" }
+
+cart.cheque.each { |product| puts product }
+
 puts "\nС Вас - #{cart.cost} руб. в т.ч. НДС. Спасибо за покупки!"
